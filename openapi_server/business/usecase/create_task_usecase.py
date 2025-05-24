1237c929d4ef16f541da49b2e300d14f2ea33a3e
shima10-x1p <moto20000203@gmail.com>
2025-05-24 17:00:26 +0900

    ToDoタスク作成ユースケースを実装し、依存性注入の設定を追加

--- openapi_server/business/usecase/create_task_usecase.py
+++ openapi_server/business/usecase/create_task_usecase.py
@@ -1,11 +1,18 @@
+"""ToDoタスク作成ユースケース."""
+
 import injector
 
 from openapi_server.business.adapters.todo_repository_postgres import TodoRepositoryPostgres
+from openapi_server.business.const import settings
 from openapi_server.business.ports.todo_repository import TodoRepository
+from openapi_server.logger import get_logger, log_function
 from openapi_server.models.task import Task
 from openapi_server.models.task_create import TaskCreate
 
+# ロガーの取得
+logger = get_logger()
 
+@log_function("INFO")
 def configure_injector() -> injector.Injector:
     """依存性注入の設定.
 
@@ -13,9 +20,8 @@ def configure_injector() -> injector.Injector:
         injector.Injector: Injectorインスタンス
 
     """
-    # PostgreSQLのDSNを指定
-    # TODO: 環境変数や設定ファイルから取得するように変更すること
-    dsn = "postgresql://user:password@localhost:5432/todo_db"
+    # PostgreSQLのDSNを設定クラスから取得
+    dsn = settings.postgres_dsn
     # Injectorを作成し、TodoRepositoryPostgresをバインド
     injector_instance = injector.Injector()
     injector_instance.binder.bind(
@@ -26,18 +32,25 @@ def configure_injector() -> injector.Injector:
     return injector_instance
 
 
-async def create_task_usecase(task_create: TaskCreate) -> Task:
+@log_function("INFO")
+async def create_task_usecase(
+    task_create: TaskCreate, injector_instance: injector.Injector = None,
+) -> Task:
     """タスクを作成するユースケース.
 
     Args:
         task_create (TaskCreate): タスク作成情報
+        injector_instance (injector.Injector, optional): Injectorインスタンス。
+            デフォルトはNoneで、configure_injector()を使用してInjectorを取得する。
 
     Returns:
         Task: 作成されたタスク
 
     """
     # Injectorを取得
-    injector_instance = configure_injector()
+    if injector_instance is None:
+        # configure_injector()を使用してInjectorを取得
+        injector_instance = configure_injector()
     # TodoRepositoryを取得
     todo_repository: TodoRepository = injector_instance.get(TodoRepository)
     # タスクを作成
